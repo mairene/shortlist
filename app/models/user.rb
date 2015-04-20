f0cd8606363caa5f8f67c7562b67195098905aed
Irene Ma <iring.ma@gmail.com>
2015-04-19 20:19:31 -0700

    add user and list model and bcrypt password hash

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+
+  include BCrypt
+
+  has_many :lists
+  validates :email, uniqueness: true
+  validates :email, format: { with: /\w+\@\w+\.\w+/ }
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+end
